67501b7e8d06cbda5a13c20131b8360b010bc27c
fubuki-no-donuts <soutakushin@gmail.com>
2024-04-28 20:56:24 +0800

    Site updated: 2024-04-28 20:56:17

---
+++ js/avatar_random.js
@@ -0,0 +1,46 @@
+var avatarimg = [
+    "https://s2.loli.net/2024/04/28/9U4mZP6gwWiptXj.jpg",
+    "https://s2.loli.net/2024/04/28/HVGIlpTrEwXeUfQ.jpg"
+  ];
+  
+  var avatarindex = 0;
+  var rotating = false;
+  
+  function changeAvatar() {
+    if (!rotating) {
+      rotating = true;
+      var avatarImgs = document.querySelectorAll(".avatar-img img");
+      avatarImgs.forEach(function(img) {
+        // 添加旋转类
+        img.classList.add("rotate");
+      });
+  
+      // 在旋转到 180 度时切换图片
+      setTimeout(function() {
+        avatarImgs.forEach(function(img) {
+          // 切换到下一个图片
+          img.src = avatarimg[avatarindex];
+        });
+      }, 250); // 与旋转一半的时间保持一致
+
+      avatarindex++;
+      if (avatarindex >= avatarimg.length) {
+        avatarindex = 0;
+      }
+      
+      // 等待旋转动画结束后再移除旋转类
+      setTimeout(function() {
+        avatarImgs.forEach(function(img) {
+          // 移除旋转类
+          img.classList.remove("rotate");
+        });
+        rotating = false;
+      }, 500); // 与动画时长保持一致
+    }
+  }
+  
+  // 初始化头像
+  changeAvatar();
+  
+  // 每隔 5 秒钟调用一次 changeAvatar 函数
+  setInterval(changeAvatar, 5000);
